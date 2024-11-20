e9fcfe93141d9148163e0f0888830317eced2746
Fillipe Augusto <fillipe484@gmail.com>
2024-11-20 19:41:16 -0300

    Initial Commit

---
+++ BaseProjectStructure/BaseProjectStructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseProjectStructureApp.swift
+//  BaseProjectStructure
+//
+//  Created by Fillipe Augusto on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BaseProjectStructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
